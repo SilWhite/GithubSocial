538eed61c97ff97b671682f19daaff2f37c9e7aa
SilWhite <144752102+SilWhite@users.noreply.github.com>
2024-11-22 13:37:57 +0800

    update

---
+++ github-social.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         github-social
+// @namespace    https://github.com/SilWhite
+// @version      0.1(2024-10-21)
+// @description  a script to set comments for github friends and repos
+// @author       SilWhite
+// @match        https://github.com/*?tab=following
+// @match        https://github.com/*?tab=repositories
+// @match        https://github.com/*?tab=followers
+
+
+
+// @grant        none
+// ==/UserScript==
+
+
+'use strict';
+
